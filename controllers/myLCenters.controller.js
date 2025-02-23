afe1fd120b8db784bd39b398b823ff1d95950bd6
Sidiqjon <sidiqjonyusufjanov7@gmail.com>
2025-02-23 19:33:18 +0500

    Bugs are being fixed

--- controllers/myLCenters.controller.js
+++ controllers/myLCenters.controller.js
@@ -1,15 +1,21 @@
 import LearningCenter from "../models/learningCenter.model.js";
+import Branch from "../models/branch.model.js";
+import User from "../models/user.model.js";
+import Region from "../models/region.model.js";
+import Comment from "../models/comment.model.js";
+import Like from "../models/like.model.js";
+import { Op, fn, col, literal } from "sequelize";
 
 export async function getMyCenters(req, res) {
     try {
        let { id } = req.user;
- 
-       let centers = await LearningCenter.findAll({ where: { createdBy: id } }, { include: { all: true } });
- 
+
+       let centers = await LearningCenter.findAll({ where: { createdBy: id } }, { attributes:{include: [[Sequelize.fn("COUNT", Sequelize.col("likes.id")), "numberOfLikes"]]}, include: { model: Branch }, include: { model: User, attributes: { exclude: ["password"] } }, include: {model: Region}, include: {model: Comment},include: {model: Like} });
+
        if (centers.length === 0) {
           return res.status(404).json({ message: "Learning centers not found! You have no Learning centers yet!" });
        }
- 
+
        res.status(200).json({ data: centers });
     } catch (error) {
        res.status(500).json({ message: error.message });
